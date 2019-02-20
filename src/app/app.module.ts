adffca1a3e3ac7c7c5f22a3442bfcd8506b44478
bennerv <10840174+bennerv@users.noreply.github.com>
2019-02-19 21:32:20 -0500

    Add routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { DetailsComponent } from './details/details.component';
+import { LocationComponent } from './location/location.component';
+
+const appRoutes: Routes = [
+  { path: '', component: DetailsComponent },
+  { path: 'locate', component: LocationComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    DetailsComponent
+    DetailsComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
